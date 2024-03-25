1c6b6eea6b3434b09b3af22320c9e3f358ae8006
Erhan Barış Ölmez <erhanbarisolmez@gmail.com>
2024-03-25 23:50:49 +0300

    jwt

---
+++ api/endpoints/user.py
@@ -0,0 +1,20 @@
+from fastapi import Depends, APIRouter, status,HTTPException
+from typing import Annotated
+from .auth import get_current_user
+from  db.database import get_db
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+user_dependency  = Annotated[dict, Depends(get_current_user)]
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db: db_dependency):
+  if user is None:
+    raise HTTPException(status_code=401, detail='Authentication Failed')
+  return {"User": user}
