e5e769dce9d5818d6cfa3dbcfaafe91319897c94
Erhan Barış Ölmez <erhanbarisolmez@gmail.com>
2024-03-30 03:43:55 +0300

    30.03.2024

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 from db.database import engine, Base
 from models import park
 from api.main import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+# CORS middleware
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
