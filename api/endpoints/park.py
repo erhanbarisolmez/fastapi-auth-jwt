4efc3913b3e6f49d9c112b1ab487f53d1ea31fd3
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-04-22 16:22:33 +0300

    22.04.2024

--- api/endpoints/park.py
+++ api/endpoints/park.py
@@ -1,11 +1,12 @@
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Annotated, List
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from api.deps import db_dependency, oauth2_bearer
-from schemas.parkSchema import CreateParkRequest
+from schemas.parkSchema import CreateParkRequest, ParkUpdateSchema
 from schemas.authSchema import CreateUserRequest
 from models.park import Park
 from .auth import login_for_access_token, get_current_user
 from datetime import timedelta, datetime
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter(prefix="/park", tags=["park"])
 
@@ -18,13 +19,7 @@ async def create_park(
 ):
 
 
-    if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token is invalid or expired.",
-        )
-
-    
+    token_HTTPException(token)
 
     create_park_model = Park(
         park_name=create_park_schema.parkName,
@@ -44,3 +39,86 @@ async def create_park(
     db.add(create_park_model)
     db.commit()
     return {"message": "Successfully created a new park."}
+
+
+
+@router.get("/read_park/{park_id}", status_code=status.HTTP_200_OK)
+async def read_park_id(
+    park_id: int,
+    token: Annotated[str, Depends(oauth2_bearer)],
+    db:db_dependency):
+    
+    token_HTTPException(token)
+    
+    park = db.query(Park).filter(Park.id == park_id).first()
+    park_HTTPException(park)
+    
+    return park
+
+
+    
+
+@router.get("/read_park_all", status_code=status.HTTP_200_OK)
+async def read_park_all(
+    db:db_dependency,
+    token: Annotated[str, Depends(oauth2_bearer)],
+):
+    
+    token_HTTPException(token)
+    
+    park_list = db.query(Park).all()
+    
+    return park_list
+
+@router.put("/update_park/{park_id}", status_code=status.HTTP_200_OK,  response_model=ParkUpdateSchema)
+async def update_park(
+    park_id: int,
+    park_data: Annotated[dict, Body(...), Depends(ParkUpdateSchema)],
+    token: Annotated[str, Depends(oauth2_bearer)],
+    db: db_dependency
+):
+    token_HTTPException(token)
+    
+    updated_park = db.query(Park).filter(Park.id == park_id).first()
+    park_HTTPException(updated_park)
+    
+    for attr, value in park_data.model_dump().items():
+        setattr(updated_park, attr, value)
+        
+        
+    
+    db.commit()
+    
+    return updated_park
+
+    
+
+@router.delete("/park/{park_id}", status_code=status.HTTP_200_OK)
+async def delete_park(
+    park_id: int,
+    token:Annotated[str, Depends(oauth2_bearer)],
+    db: db_dependency):
+    
+    token_HTTPException(token)
+    
+    park = db.query(Park).filter(Park.id == park_id).first()
+    park_HTTPException(park_id)
+    
+    db.delete(park)
+    db.commit()
+    
+    return park
+
+def token_HTTPException(token):
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token is invalid or expired.",
+        )
+
+def park_HTTPException(park):
+    if not park:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Park not found"
+        )
--- schemas/parkSchema.py
+++ schemas/parkSchema.py
@@ -3,17 +3,35 @@ from datetime import datetime
 from typing import Optional
 
 class CreateParkRequest(BaseModel):
-  parkName: str
-  lat: float
-  lng: float
-  capacity: int
-  emptyCapacity: int
-  workHours: str
-  parkType: str
-  freeTime: int
-  district: str
-  isOpen: bool
-  city: str
-  enable: bool
+  parkName: str | None = None
+  lat: float | None = None
+  lng: float | None = None
+  capacity: int | None = None
+  emptyCapacity: int | None = None
+  workHours: str | None = None
+  parkType: str | None = None
+  freeTime: int | None = None
+  district: str | None = None
+  isOpen: bool | None = None
+  city: str | None = None
+  enable: bool | None = None
   registeredDate: Optional[datetime]
+  
+  
+class ParkUpdateSchema(BaseModel):
+  parkName: Optional[str | None] = None
+  lat: Optional[float | None] = None
+  lng: Optional[float | None] = None
+  capacity: Optional[int | None] = None
+  emptyCapacity: Optional[int | None] = None
+  workHours: Optional[str | None] = None
+  parkType: Optional[str | None] = None
+  freeTime: Optional[int | None] = None
+  district: Optional[str | None] = None
+  isOpen: Optional[bool | None] = None
+  city: Optional[str | None] = None
+  enable: Optional[bool | None] = None
+  registeredDate: Optional[datetime] = None
+  
+
   
